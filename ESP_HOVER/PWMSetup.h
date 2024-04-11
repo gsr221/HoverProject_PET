bcbfaf4a701270aee17a6523c2f5fa7fac973869
Gabriel Ribeiro <138245389+gsr221@users.noreply.github.com>
2024-04-10 23:56:02 -0300

    Delete ESP_HOVER/PWMSetup.h

--- ESP_HOVER/PWMSetup.h
+++
@@ -1,9 +0,0 @@
-#include "Consts.h"
-
-//FUNCTION TO SETUP PWM IN ESP32
-void SetupPWM(){
-  ledcSetup(RPWM_CHAN, PWM_FREQ, PWM_RES);
-  ledcSetup(LPWM_CHAN, PWM_FREQ, PWM_RES);
-  ledcAttachPin(RPWM_PIN, RPWM_CHAN);
-  ledcAttachPin(LPWM_PIN, LPWM_CHAN);
-}
